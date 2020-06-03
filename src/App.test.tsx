5ae87fc9a93a95f5931392b2fb3f9001f507e4c9
siluat <siluat@gmail.com>
2020-06-04 04:45:15 +0900

    🎉 프로젝트 시작

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/구브라 환상도서관/i);
   expect(linkElement).toBeInTheDocument();
 });
