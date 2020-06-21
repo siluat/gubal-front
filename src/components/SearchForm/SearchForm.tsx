925564a64837a8521d3ca87937a1f8ff48ef4a75
Siluat <siluat@gmail.com>
2020-06-21 18:17:37 +0900

    💄 검색어 입력 폼 스타일 수정

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -5,24 +5,31 @@ import Icon from '../Icon/Icon';
 function SearchForm() {
   return (
     <StyledForm>
-      <Icon type="find" />
+      <Icon type="find" color={placeholderColor} />
       <StyledInput aria-label="검색어" placeholder="검색어를 입력하세요" />
     </StyledForm>
   );
 }
+
+const placeholderColor = '#adb5bd';
 
 const StyledForm = styled.form`
   display: flex;
   flex-direction: row;
   flex-grow: 1;
   align-items: center;
+  background-color: white;
+  padding-left: 0.7rem;
 `;
 
 const StyledInput = styled.input`
   border: none;
   outline: none;
-  padding: 1rem;
+  padding: 1rem 1rem 0.9rem 0.7rem;
   font-size: 1.1rem;
+  ::placeholder {
+    color: ${placeholderColor};
+  }
 `;
 
 export default SearchForm;
