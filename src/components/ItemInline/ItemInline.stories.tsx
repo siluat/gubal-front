3cdbd3c22ecccf454bf3927e910927e13c684404
Siluat <siluat@gmail.com>
2020-07-12 19:10:26 +0900

    💄 ItemInline UI 구성

--- src/components/ItemInline/ItemInline.stories.tsx
+++ src/components/ItemInline/ItemInline.stories.tsx
@@ -1,23 +1,50 @@
 import React from 'react';
 import ItemInline from './ItemInline';
 import { ItemSummary } from '../../modules/library';
+import { decodeIcon } from '../../utils/decoder';
 
 export default {
   component: ItemInline,
   title: 'components/ItemInline',
 };
 
-const testItemSummary: ItemSummary = {
-  id: 10057,
-  name: '롱기누스: 제타',
-  icon: '31887',
-  itemLevel: 135,
-  rarity: 4,
-  category: 5,
-  equipLevel: 50,
-};
+const testItemSummaries: ItemSummary[] = [
+  {
+    id: 1874,
+    name: '미완성 게 볼그',
+    icon: '31843',
+    itemLevel: 50,
+    rarity: 1,
+    category: 5,
+    equipLevel: 50,
+  },
+  {
+    id: 9494,
+    name: '롱기누스',
+    icon: '31887',
+    itemLevel: 125,
+    rarity: 4,
+    category: 5,
+    equipLevel: 50,
+  },
+  {
+    id: 10057,
+    name: '롱기누스: 제타',
+    icon: '31887',
+    itemLevel: 135,
+    rarity: 4,
+    category: 5,
+    equipLevel: 50,
+  },
+];
 
-export const itemInline = () => <ItemInline item={testItemSummary} />;
+export const itemInline = () => <ItemInline item={testItemSummaries[0]} />;
 itemInline.story = {
   name: 'Default',
+};
+
+export const itemInlineList = () =>
+  testItemSummaries.map(i => <ItemInline item={i} />);
+itemInlineList.story = {
+  name: 'List',
 };
--- src/components/ItemInline/ItemInline.tsx
+++ src/components/ItemInline/ItemInline.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styled from '@emotion/styled';
 import { ItemSummary } from '../../modules/library';
+import { decodeIcon, decodeItemCategory } from '../../utils/decoder';
 
 const ItemInlineBlock = styled.div`
   display: flex;
   box-sizing: border-box;
   height: 60px;
+  border-bottom: 1px solid gray;
   .icon-column {
     padding: 10px;
     .icon {
@@ -13,19 +15,64 @@ const ItemInlineBlock = styled.div`
       height: 40px;
     }
   }
+  .description-column {
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    .name {
+      font-size: 0.9rem;
+      margin: 0.1rem 0;
+    }
+    .level {
+      font-size: 0.7rem;
+      display: flex;
+      margin: 0.1rem 0;
+      dd {
+        margin: 0 1rem;
+      }
+    }
+  }
+  .category-column {
+    width: 50px;
+    min-width: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 type ItemInlineProps = {
   item: ItemSummary;
 };
 
+const baseUrl = 'https://gubal.s3.ap-northeast-2.amazonaws.com';
+
 function ItemInline({ item }: ItemInlineProps) {
-  const { name } = item;
+  const { name, icon, itemLevel, equipLevel, category } = item;
+  const iconUrl = useMemo(() => `${baseUrl}${decodeIcon(icon)}`, [icon]);
+  const { name: categoryName, icon: categoryIcon } = decodeItemCategory(
+    category,
+  );
+  const categoryIconUrl = `${baseUrl}${categoryIcon}`;
   return (
     <ItemInlineBlock>
-      <div className="icon-column"></div>
-      <div className="description-column">{name}</div>
-      <div className="category-column"></div>
+      <div className="icon-column">
+        <img className="icon" src={iconUrl} alt={`${name} 아이콘`} />
+      </div>
+      <div className="description-column">
+        <p className="name">{name}</p>
+        <dl className="level">
+          <dt>착용 레벨</dt>
+          <dd>{equipLevel}</dd>
+          <dt>아이템 레벨</dt>
+          <dd>{itemLevel}</dd>
+        </dl>
+      </div>
+      <div className="category-column">
+        <img src={categoryIconUrl} alt={categoryName} />
+      </div>
     </ItemInlineBlock>
   );
 }
