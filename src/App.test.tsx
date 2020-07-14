3fce954c1a7100de762eb6d3f894daf18ac5c796
siluat <siluat@gmail.com>
2020-07-15 01:32:11 +0900

    🏗 첫 페이지를 검색 페이지로 변경

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,9 +3,12 @@ import { MemoryRouter } from 'react-router-dom';
 import { render, screen, wait } from './utils/test-utils';
 import App, { AppRoute } from './App';
 
-test('렌더링', () => {
-  const { container } = render(<App />);
-  expect(container).toBeInTheDocument();
+test('렌더링', async () => {
+  render(<App />);
+
+  await wait(() =>
+    expect(screen.getByTestId('search-page')).toBeInTheDocument(),
+  );
 });
 
 test('route /search', async () => {
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
 import React, { useEffect } from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import FullScreen from './layouts/FullScreen';
-import JobIconMorph from './components/JobIconMorph/JobIconMorph';
-import styled from '@emotion/styled';
 import Search from './pages/Search';
 import { useDispatch } from 'react-redux';
 import { getItemSummariesAsync } from './modules/library';
 
-const Text = styled.span`
-  font-size: 1rem;
-  color: #bbac94;
-  line-height: 3rem;
-`;
-
 export function AppRoute() {
   return (
     <Switch>
-      <Route exact path="/">
-        <FullScreen>
-          <JobIconMorph color="#bbac94" />
-          <Text>서비스 오픈 준비 중</Text>
-        </FullScreen>
-      </Route>
-      <Route exact path="/search">
+      <Route exact path={['/', '/search']}>
         <Search />
       </Route>
     </Switch>
