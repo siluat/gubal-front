7dc3d0e5bc5af88affabcf48c91a404fae379561
siluat <siluat@gmail.com>
2020-07-23 20:56:14 +0900

    👽 getItemDetail API 추가

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,4 +2,9 @@ import axios from 'axios';
 
 const client = axios.create();
 
+const AWS_S3_ENDPOINT = `https://gubal.s3.ap-northeast-2.amazonaws.com`;
+
 export const getItemSummaries = () => client.get('/data/ItemSummaries.json');
+
+export const getItemDetail = (id: number) =>
+  client.get(`${AWS_S3_ENDPOINT}/data/item/${id}.json`);
